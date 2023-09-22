4799e19a62919329deecb14d6c46332fa213ef14
Maitreya Gupta <95370078+MaitreyaGupta@users.noreply.github.com>
2023-09-22 22:29:25 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,63 @@
+const express=require("express")
+const app=express() 
+const bodyparser=require("body-parser")
+const mongodb=require("./config/mongoose")
+const User=require("./models/user")
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.set("view engine","ejs")
+ 
+//const expressLayouts=require("./views/ExpressLayouts")
+//app.use(expressLayouts)
+const port=process.env.port||3030
+
+app.listen(port,function(req,res){
+    console.log("Listening")
+})
+
+app.get("/",function(req,res){
+    res.render("user_sign_in")
+})
+app.get("/signup",function(req,res){
+    res.render("user_sign_up")
+})
+
+app.post("/",async function(req,res,err){
+const data={
+        email:req.body.email,
+        password:req.body.password,
+    }
+    const check=await User.findOne({email:req.body.email})
+    const check1=await User.findOne({password:req.body.password})
+    if(check==null && check1==null)
+    {
+        User.insertMany([data])
+        res.redirect("/")
+    }
+    else{
+        res.send("Duplicate name or password")
+    }
+ 
+})
+
+app.post("/signup",async function(req,res){
+    try
+    {
+     const check=await User.findOne({email:req.body.email})
+     if(check.password===req.body.password)
+     {
+        res.render("Index")
+     }
+     else
+     {
+        res.send("Wrong information")
+     }
+    }
+    catch{
+        res.send("Wrong details")
+    }
+})
+
+app.get("/Index",function(req,res){
+    res.render("Index")
+})
