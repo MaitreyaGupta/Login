a30fb6ed22f36c63f415fcf81aedb99c12eec586
Maitreya Gupta <95370078+MaitreyaGupta@users.noreply.github.com>
2023-09-23 22:51:37 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/codedial_developement",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect("mongodb+srv://maitreyaguptaa:killerman@devs.syeiknh.mongodb.net/ab",{useNewUrlParser:true,useUnifiedTopology:true})
+
 
 const db=mongoose.connection
 
